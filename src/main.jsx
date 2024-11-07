993219f3283145faba3b22f5998ba5639720a272
dizzydog-rgb <fox29582@yahoo.com.tw>
2024-11-07 17:21:12 +0800

    更新 variables.scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import "./styles/global.scss";
 import "../node_modules/bootstrap/js/index.esm.js";
 import App from "./App.jsx";
