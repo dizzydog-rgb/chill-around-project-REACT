a7b7378d8f36b133f11479329c9da742121d9e5a
dizzydog-rgb <fox29582@yahoo.com.tw>
2024-11-20 10:16:58 +0800

    npm run dev 自動開啟網頁

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   // base: "/chill-around-project-REACT/",
   plugins: [react()],
+  server: {
+    open: true, // 自動開啟瀏覽器
+  },
 });
