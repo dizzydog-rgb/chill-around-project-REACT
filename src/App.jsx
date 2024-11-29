5b2c859e5be2f195efadb8027094d89f01bbe263
hsinya22 <v848clmp20@gmail.com>
2024-11-29 10:04:08 +0800

    解決衝突

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,27 @@ import "./App.scss";
 import React, { Component } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "./pages/indexPage/index.jsx";
+import SchInfo from "./pages/schInfoPage/schInfo.jsx";
+import BuildPlan from "./pages/BuildPlanPage/BuildPlanPage.jsx";
+import EditPlan from "./pages/EditPlanPage/EditPlanPage.jsx";
+import PlanList from "./pages/PlanListPage/PlanListPage.jsx";
 import SearchSite from "./pages/searchSitePage/searchSitePage.jsx";
 import SiteInfo from "./pages/siteInfoPage/SiteInfoPage.jsx";
 
 function App() {
   return (
-    // {<>
-    //   <div>我是首頁</div>
-    //   <button type="button" className="btn btn-primary">
-    //     Primary
-    //   </button>
-    //   <i className="bi bi-6-circle-fill"></i>
-    // </>}
     <Router>
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/searchSite" element={<SearchSite />} />
-      <Route path="/siteInfo/:id" element={<SiteInfo />} />
-      <Route path="*" element={<h1>找不到頁面</h1>} />
-    </Routes>
-  </Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/schInfo" element={<SchInfo/>} />
+        <Route path="*" element={<h1>找不到頁面</h1>} />
+        <Route path="/bulidPlan" element={<BuildPlan />} />
+        <Route path="/editPlan" element={<EditPlan />} />
+        <Route path="/planList" element={<PlanList />} />
+        <Route path="/searchSite" element={<SearchSite />} />
+        <Route path="/siteInfo/:id" element={<SiteInfo />} />
+      </Routes>
+    </Router>
   );
 }
 
