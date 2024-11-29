9f51f963afba7da113d3bf4a8da61f5205fef8cd
HSUAN0913 <k115599159@gmail.com>
2024-11-29 11:03:17 +0900

    檔案名稱更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
 import "./App.scss";
 import React, { Component } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Index from "./pages/indexPage/index.jsx";
-import SchInfo from "./pages/schInfoPage/schInfo.jsx";
-
-function App() {
-  return (
-    // {<>
-    //   <div>我是首頁</div>
-    //   <button type="button" className="btn btn-primary">
-    //     Primary
-    //   </button>
-    //   <i className="bi bi-6-circle-fill"></i>
-    // </>}
-  <Router>
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/schInfo" element={<SchInfo/>} />
-      <Route path="*" element={<h1>找不到頁面</h1>} />
-    </Routes>
-  </Router>
+import Index from "./pages/indexPage/Index.jsx";
+import SchInfo from "./pages/schInfoPage/SchInfo.jsx";
 import BuildPlan from "./pages/BuildPlanPage/BuildPlanPage.jsx";
 import EditPlan from "./pages/EditPlanPage/EditPlanPage.jsx";
 import PlanList from "./pages/PlanListPage/PlanListPage.jsx";
@@ -29,6 +12,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/schInfo" element={<SchInfo/>} />
         <Route path="*" element={<h1>找不到頁面</h1>} />
         <Route path="/bulidPlan" element={<BuildPlan />} />
         <Route path="/editPlan" element={<EditPlan />} />
